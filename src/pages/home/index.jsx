bfc21dd6728134594563e5a4a31ce1e042f94c07
Siqm <rafaelsmello01@gmail.com>
2023-10-12 12:51:17 -0300

    [50%] welcome, sobre mim e skills

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,16 +1,79 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import siqLogo from '../../assets/siqe-logo.svg'
+import styles from './styles.module.scss'
+import blackInstaIcon from '../../assets/instagram-black.svg'
+import rafaPng from '../../assets/avatar-rafa.png'
+import devices from '../../assets/welcome-devices.svg'
 
 export default function Home() {
 
-    return(
-        <>
-            <header>
-                <img src={siqLogo} alt="" />
+  // eslint-disable-next-line
+  const [whiteMode, setWhiteMode] = useState({})
+
+  useEffect(() => {
+
+    setWhiteMode({
+      icons: {
+        instagram: blackInstaIcon,
+      }
+    })
+  }, [])
+
+  return (
 
 
-            </header>
 
-        </>
-    )
+    <>
+      <header className={styles.header}>
+
+        <div className={styles.container}>
+
+          <img className={styles.logo} src={siqLogo} alt="" />
+
+          <nav>
+            <button>claro/escuro</button>
+
+            <button>Contato</button>
+          </nav>
+        </div>
+      </header>
+
+
+
+      <section className={styles.welcome}>
+
+        <div className={`${styles.container} ${styles.welcomeBody}`}>
+          <h1>Fullstack developer & DevOps</h1>
+          <h2>I develop inovative solutions to integrate teams and write clean code</h2>
+          <img src={rafaPng} alt="Foto ilustrativa" />
+
+        </div>
+
+        <div className={styles.welcomeFooter}>
+          <img className={styles.devices} src={devices}/>
+
+        </div>
+      </section>
+
+      <section className={styles.mediumSection}>
+        <div className={styles.container}>
+          <h2>Hi, I'm Rafael. Welcome to my Portfolio</h2>
+          <p>Lorem ipsum</p>
+        </div>
+      </section>
+
+      <section className={styles.skillsSection}>
+        <div className={styles.container}>
+          <div className={styles.box}>
+            <div className={styles.column}>
+              
+            </div>
+            <div className={styles.column}></div>
+            <div className={styles.column}></div>
+          </div>
+        </div>
+      </section>
+
+    </>
+  )
 }
