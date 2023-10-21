864c4417235c8c251f690e53fb441813ab3771a2
Siqm <rafaelsmello01@gmail.com>
2023-10-21 16:26:58 -0300

    [Estrutura] Contato, projetos e habilidades

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,8 @@ import styles from './styles.module.scss'
 import blackInstaIcon from '../../assets/instagram-black.svg'
 import rafaPng from '../../assets/avatar-rafa.png'
 import devices from '../../assets/welcome-devices.svg'
+import iconFrontend from '../../assets/front-end-1.svg'
+import iconBackend from '../../assets/database-1.svg'
 
 export default function Home() {
 
@@ -65,14 +67,58 @@ export default function Home() {
       <section className={styles.skillsSection}>
         <div className={styles.container}>
           <div className={styles.box}>
+
             <div className={styles.column}>
-              
+              <img src={iconBackend} alt="" />
+              <h3>Back-end</h3>
+              <p>I value simple content structure, clean design patterns, and thoughtful interactions</p>
+              <p>Languages and tools</p>
+              <p>Node.js, Lua, MySQL, MongoDB</p>
             </div>
-            <div className={styles.column}></div>
-            <div className={styles.column}></div>
+
+            <div className={styles.column}>
+              <img src={iconFrontend} alt="" />
+              <h3>Front-end</h3>
+              <p>I like to code things from scratch, and enjoy bringing ideas to life in the browser.</p>
+              <p>Languages and tools</p>
+              <p>React, React-Native, Sass, Javascript, Typescript</p>
+            </div>
+
+            <div className={styles.column}>
+              <h3>Outras Habilidades</h3>
+              <p>To increse productive and team integration I also work with</p>
+              <p>Github, Git, Postman</p>
+            </div>
           </div>
         </div>
       </section>
+
+      <section>
+        <div className={styles.container}>
+          <h3>Latest projects I've worked on</h3>
+          
+        </div>
+      </section>
+
+      <section>
+        <div className={styles.container}>
+          <h3>Contact me</h3>
+          <form>
+            <label>Name</label>
+            <input type="text" />
+
+            <label>Email</label>
+            <input type="email" />
+
+            <label>Message</label>
+            <input type="text" />
+          </form>
+        </div>
+      </section>
+
+      <footer>
+        <p>Developed by Siq Enterprise</p>
+      </footer>
 
     </>
   )
